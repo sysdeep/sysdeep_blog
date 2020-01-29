cd7ac7b77265f6ebcb9d2620b8d9314856d96a4b
Diver Dune <sysdeep@yandex.ru>
2020-01-29 16:31:12 +0300

    remake images

--- content/blog/msdos_1.1/msdos.md
+++ content/blog/msdos_1.1/msdos.md
@@ -1,18 +1,17 @@
 ﻿Title: MS DOS 1.1
 Date: 2014-12-01 20:01
 Tags: history, os, dos, ms
+Slug: history-msdos-1_1
 
 
 
 
 Экраны загруженной в виртуальной машине MS DOS 1.1
 
-
-![Alt text]({attach}1.jpg)
-
-
-![Alt text]({attach}2.jpg)
+![Alt text]({static}/images/ms_dos-1.1/1.jpg)
+![Alt text]({static}/images/ms_dos-1.1/2.jpg)
 
 
----
+
+
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -21,16 +21,21 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 25
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+
+
+
+USE_FOLDER_AS_CATEGORY = False
